99fcbaa595e749ac687e2ad24ed0ec31da1b6ec2
mrgirish21 <mrgirish21@gmail.com>
2022-06-18 11:21:02 +1000

    cognito settings

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-2_R5t20I6Fe", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "3lf7d2qf1nso89fj1630g7qd5a", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
